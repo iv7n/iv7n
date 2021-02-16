4c686076c8f312ca37aec2461f0696b32c39372b
yeojongki <kk597@sina.com>
2021-02-16 23:09:14 +0800

    chore: add commitlint & format code

--- packages/utils/src/format/currency.ts
+++ packages/utils/src/format/currency.ts
@@ -4,15 +4,15 @@
  * @param showSymbol - 是否显示 $ 符号 默认显示 @defaultValue `true`
  */
 export function formatCurrency(
-	value: string | number = 0.0,
-	showSymbol = true,
+  value: string | number = 0.0,
+  showSymbol = true
 ) {
-	value = value + ''
-	const index = value.indexOf('-')
-	const prefix = showSymbol ? '$' : ''
-	if (index === -1) {
-		return prefix + parseFloat(value).toFixed(2)
-	} else {
-		return '-' + prefix + parseFloat(value.split('-')[1]).toFixed(2)
-	}
+  value = value + ''
+  const index = value.indexOf('-')
+  const prefix = showSymbol ? '$' : ''
+  if (index === -1) {
+    return prefix + parseFloat(value).toFixed(2)
+  } else {
+    return '-' + prefix + parseFloat(value.split('-')[1]).toFixed(2)
+  }
 }
