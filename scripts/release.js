09402e912fd5de6d0d58f5485d81b04f7d570f7e
yeojongki <yeojongki@foxmail.com>
2021-02-17 21:13:20 +0800

    chore: update release script

--- scripts/release.js
+++ scripts/release.js
@@ -6,7 +6,10 @@ const execa = require('execa')
 
 const skipTests = args.skipTests
 const skipBuild = args.skipBuild
+const fromPackage = args.fromPackage
+const fromGit = args.fromGit
 
+const bin = name => path.resolve(__dirname, '../node_modules/.bin/' + name)
 const run = (bin, args, opts = {}) =>
   execa(bin, args, { stdio: 'inherit', ...opts })
 const step = msg => console.log(chalk.cyan(msg))
@@ -18,7 +21,7 @@ async function main() {
     await run(bin('jest'), ['--clearCache'])
     await run('yarn', ['test'])
   } else {
-    console.log(`(skipped)`)
+    console.log('(skipped)')
   }
 
   // build all packages with types
@@ -29,33 +32,49 @@ async function main() {
     // step('\nVerifying type declarations...')
     // await run('yarn', ['test-dts-only'])
   } else {
-    console.log(`(skipped)`)
+    console.log('(skipped)')
   }
+
+  // publish packages
+  step('\nPublishing packages...')
+  await run('lerna', [
+    'publish',
+    '--no-push',
+    fromPackage ? '--from-package' : '',
+    fromGit ? '--from-git' : '',
+  ])
 
   // update all package versions and inter-dependencies
   step('\nUpdating all package versions...')
-  await updateVersions()
+  const version = await updateVersions()
 
   // generate changelog
-  await run(`yarn`, ['changelog'])
+  await run('yarn', ['changelog'])
+
+  step('\nCommitting changes...')
+  await runIfNotDry('git', ['add', '-A'])
+  await runIfNotDry('git', ['commit', '-m', `chore(release): v${version}`])
+
+  step(`v${version} is released 🎉`)
 }
 
 async function updateVersions() {
-  // update all package versions
-  await run(`lerna`, ['version', '--no-git-tag-version'])
+  // // update all package versions
+  // await run(`lerna`, ['version', '--no-git-tag-version'])
 
   // update self package version
   // get latest version
-  const pkg = fs.readFileSync(
-    path.resolve(__dirname, '../packages', 'lerna.json'),
-    'utf-8'
+  const pkg = JSON.parse(
+    fs.readFileSync(path.resolve(__dirname, '../', 'lerna.json'), 'utf-8')
   )
   const { version } = pkg
 
   const selfPkgPath = path.resolve(__dirname, '../', 'package.json')
-  const selfPkg = fs.readFileSync(selfPkgPath, 'utf-8')
+  const selfPkg = JSON.parse(fs.readFileSync(selfPkgPath, 'utf-8'))
   selfPkg.version = version
   fs.writeFileSync(selfPkgPath, JSON.stringify(selfPkg, null, 2) + '\n')
+
+  return version
 }
 
 main().catch(err => {
